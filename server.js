42f38a1f92d11c7608d142eaa9ed4c11da1e099c
knanalmnswry06-collab <knanalmnswry06@gmail.com>
2026-06-01 03:49:59 +0300

    Initialize Express server with API routing

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const api = require("./api");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use("/api", api);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
